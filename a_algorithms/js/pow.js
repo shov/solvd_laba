11a80293beae9bd4f395cf33a7bac2ccdaa1fc67
Alexandr Shevchenko <ls.shov@gmail.com>
2021-03-31 18:58:01 +0300

    braces

--- a_algorithms/js/pow.js
+++ a_algorithms/js/pow.js
@@ -4,55 +4,121 @@ function assert(a) {
   }
 }
 
-const LOG = false
+const LOG = true
 function log(...a) {
   !LOG || console.log(...a)
 }
 
 function powOn(x, n) {
-	return n == 0 ? 1 : new Array(  Math.floor(n)  )
-				.fill(x)
-.reduce((acc, _) => acc * x)
+  let start = process.hrtime()
+  start = start[0] * 1000000 + start[1]
+
+  let result = 1
+  let counter = n
+
+  while (counter > 0) {
+    result *= x
+    counter--
+  }
+
+  let finish = process.hrtime()
+  finish = finish[0] * 1000000 + finish[1]
+  log(`T:${finish - start}`)
+  return result
 }
 
 assert(powOn(2, 0)  === Math.pow(2, 0))
 assert(powOn(2, 1)  === Math.pow(2, 1))
 assert(powOn(2, 4)  === Math.pow(2, 4))
 assert(powOn(10, 2) === Math.pow(10, 2))
-assert(powOn(3, 3)  === Math.pow(3, 3))
+assert(powOn(3, 3) === Math.pow(3, 3))
+log('-')
+assert(powOn(3, 32)  === Math.pow(3, 32))
+
+log(`-------------------------`)
+
+function powReduce(x, n) {
+  let start = process.hrtime()
+  start = start[0] * 1000000 + start[1]
+
+	const result =  n == 0 ? 1 : new Array(  Math.floor(n)  )
+				.fill(x)
+    .reduce((acc, _) => acc * x)
+
+  let finish = process.hrtime()
+  finish = finish[0] * 1000000 + finish[1]
+  log(`T:${finish - start}`)
+  return result
+}
+
+assert(powReduce(2, 0)  === Math.pow(2, 0))
+assert(powReduce(2, 1)  === Math.pow(2, 1))
+assert(powReduce(2, 4)  === Math.pow(2, 4))
+assert(powReduce(10, 2) === Math.pow(10, 2))
+assert(powReduce(3, 3) === Math.pow(3, 3))
+log('-')
+assert(powReduce(3, 32)  === Math.pow(3, 32))
+
+log(`-------------------------`)
 
 function powSq(x, n) {
+  let start = process.hrtime()
+  start = start[0] * 1000000 + start[1]
+
   let result = 1
   let xInPowOf2 = x
 
-  log(`init n=0b${n.toString(2)}`)
-  log(`init r=${result}`)
-  log(`init p2=${xInPowOf2}`)
-
-
   while (n > 0) {
-    log('')
 
     if (n & 1) {
       result *= xInPowOf2
-      log(`n & 1 r=${result}`)
     }
 
     xInPowOf2 *= xInPowOf2
-    log(`p2=${xInPowOf2}`)
     n = n >> 1 //or n /= 2
-    log(`n >> =0b${n.toString(2)}`)
   }
 
-  log('')
-  log(`Done, res = ${result}`)
+  let finish = process.hrtime()
+  finish = finish[0] * 1000000 + finish[1]
+  log(`T:${finish - start}`)
   return result
 }
-//console.log(powSq(3, 13))
+//log(powSq(3, 13))
 assert(powSq(2, 0)  === Math.pow(2, 0))
 assert(powSq(2, 1)  === Math.pow(2, 1))
 assert(powSq(2, 4)  === Math.pow(2, 4))
 assert(powSq(10, 2) === Math.pow(10, 2))
-assert(powSq(3, 3)  === Math.pow(3, 3))
+assert(powSq(3, 3) === Math.pow(3, 3))
+log('-')
+assert(powSq(3, 32)  === Math.pow(3, 32))
 
-console.log(`Done`)
+log(`Done`)
+
+
+
+function sqReducePow(x, n) {
+  let start = process.hrtime()
+  start = start[0] * 1000000 + start[1]
+
+  return new Array(n).fill(x).reduce((acc, x) => {
+
+    return acc
+  }, { r: 1 , p2 = x}).r
+  let result = 1
+  let xInPowOf2 = x
+
+  while (n > 0) {
+
+    if (n & 1) {
+      result *= xInPowOf2
+    }
+
+    xInPowOf2 *= xInPowOf2
+    n = n >> 1 //or n /= 2
+  }
+
+  let finish = process.hrtime()
+  finish = finish[0] * 1000000 + finish[1]
+  log(`T:${finish - start}`)
+  return result
+}
